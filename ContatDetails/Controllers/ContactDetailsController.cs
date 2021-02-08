6dd269d0b3de89a58baab7be29fea610f4ea8318
Thushara Jose <thushara.jose123@gmail.com>
2021-02-08 20:16:05 +1000

    Firstcommit

---
+++ ContatDetails/Controllers/ContactDetailsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContatDetails.Controllers
+{
+    public class ContactDetailsController : Controller
+    {
+        private readonly ContactDetailsEntities db;
+     
+        public ContactDetailsController(ContactDetailsEntities db)
+        {
+            this.db = db;
+        }
+        public ActionResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View(db.Contacts.Where(x=>x.Id ==id).FirstOrDefault());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Contacts.Add(contact);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+
+                Contact contact = db.Contacts.Find(id);
+                if (contact == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(db.Contacts.Where(x => x.Id == id).FirstOrDefault());
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Contact contact)
+        {
+            try
+            {  
+                if (ModelState.IsValid)
+                {
+                    db.Entry(contact).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(db.Contacts.Where(x=>x.Id==id).FirstOrDefault());
+            }
+        }
+
+       [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(db.Contacts.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Contact contact)
+        {
+            try
+            {
+                Contact contacts = db.Contacts.Find(id);
+                db.Contacts.Remove(contacts);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ContatDetails/Global.asax.cs
@@ -0,0 +1,70 @@
+﻿using Autofac;
+using Autofac.Integration.Web;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace ContatDetails
+{
+    public class MvcApplication : System.Web.HttpApplication, IContainerProviderAccessor
+    {
+        private static IContainer _container;
+        private static IContainerProvider _containerProvider;
+
+        // Instance property that will be used by Autofac HttpModules
+        // to resolve and inject dependencies.
+        public IContainerProvider ContainerProvider => _containerProvider;
+
+        public object IocConfig { get; private set; }
+
+        protected void Application_Start()
+        {
+          
+            AreaRegistration.RegisterAllAreas();
+
+            var DisplayMode = DisplayModeProvider.Instance.Modes;
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("WP")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent()
+                .IndexOf("Windows Phone OS", StringComparison.OrdinalIgnoreCase) > 0)
+
+            });
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("iPhone")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent()
+                .IndexOf("iPhone", StringComparison.OrdinalIgnoreCase) > 0)
+
+            });
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("Android")
+            {
+                ContextCondition = (ctx => ctx.GetOverriddenUserAgent()
+                .IndexOf("Android", StringComparison.OrdinalIgnoreCase) > 0)
+
+            });
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+         
+            var builder = new ContainerBuilder();
+          
+            builder.RegisterControllers(typeof(MvcApplication).Assembly)
+                       .PropertiesAutowired();
+            builder
+               .RegisterType<ContactDetailsEntities>()
+               .AsSelf()
+               .InstancePerRequest();
+            builder.RegisterType<ContactDetailsEntities>().As<DbContext>();
+  
+            _container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(_container));
+
+        }
+    }
+}
